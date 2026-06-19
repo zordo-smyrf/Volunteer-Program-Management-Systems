01853388656ec76829c961d3ccbd9adead3fa861
zordo-smyrf <danilstelle32@gmail.com>
2026-06-20 01:35:51 +0600

    Ready App29

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log("Server started on " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server started on port " + PORT);
 });
